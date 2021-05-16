fc7369e7baa509a05e894633e6356295ad71fe3f
unknown <williamcaetano14@gmail.com>
2021-05-16 07:50:05 -0300

    :white_check_mark: Added test for optional parameter

--- src/components/ConditionalRenderer/ConditionalRenderer.test.tsx
+++ src/components/ConditionalRenderer/ConditionalRenderer.test.tsx
@@ -2,11 +2,27 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ConditionalRenderer from './ConditionalRenderer';
 
-test('ConditionalRenderer success case', () => {
+test('ConditionalRenderer success case without optional parameter', () => {
   const text = 'La Li Lu Le Lo';
   render(<ConditionalRenderer condition onSuccess={() => <p>{text}</p>} />);
   const element = screen.getByText(text);
   expect(element).toBeInTheDocument();
+});
+
+test('ConditionalRenderer success case with optional parameter', () => {
+  const textSuccess = 'Chrono';
+  const textFailure = 'Trigger';
+  render(
+    <ConditionalRenderer
+      condition
+      onSuccess={() => <p>{textSuccess}</p>}
+      onFailure={() => <p>{textFailure}</p>}
+    />,
+  );
+  const elementOnSuccess = screen.getByText(textSuccess);
+  const elementOnFailure = screen.queryByText(textFailure);
+  expect(elementOnSuccess).toBeInTheDocument();
+  expect(elementOnFailure).not.toBeInTheDocument();
 });
 
 test('ConditionalRenderer failure case', () => {
