32391540aab9e87d93134e2da1acb4d01af6cedd
unknown <williamcaetano14@gmail.com>
2021-05-17 19:50:55 -0300

    :white_check_mark: Created test to check if fallback function render correctly

--- src/components/Iterator/Iterator.test.tsx
+++ src/components/Iterator/Iterator.test.tsx
@@ -26,3 +26,17 @@ test('Iterator should not render anything if list is empty', () => {
   const element = screen.queryByRole('p');
   expect(element).not.toBeInTheDocument();
 });
+
+test('Iterator should render fallback function', () => {
+  const list = [];
+  const text = "Here I'm";
+  render(
+    <Iterator
+      list={list}
+      onRender={(prop, index) => <p key={index}>{prop}</p>}
+      onFallback={() => <p>{text}</p>}
+    />,
+  );
+  const element = screen.queryByText(text);
+  expect(element).toBeInTheDocument();
+});
