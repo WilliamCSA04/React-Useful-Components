55c4f61b5f74f82c05b735ad38d6a7d5ef34c7f4
unknown <williamcaetano14@gmail.com>
2021-05-17 20:38:00 -0300

    :memo: Added documentation to Conditional Renderer

--- src/components/ConditionalRenderer/ConditionalRenderer.types.ts
+++ src/components/ConditionalRenderer/ConditionalRenderer.types.ts
@@ -1,3 +1,9 @@
+/**
+ * @interface ConditionalRendererPropTypes
+ * @param condition Receives a truthy or falsy value
+ * @param onSuccess JSX function that executes if condition has a truthy value
+ * @param onFailure JSX function that executes if condition has a truthy falsy
+ */
 export interface ConditionalRendererPropTypes {
   condition: unknown;
   onSuccess: () => JSX.Element;
