d37538a2eb3e22507ee23d99966989460d862c15
unknown <williamcaetano14@gmail.com>
2021-05-19 23:43:08 -0300

    :sparkles: Added basic logic for Repeater

--- src/components/Repeater/Repeater.tsx
+++ src/components/Repeater/Repeater.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
+import validator from '../helpers';
 import { RepeaterPropTypes } from './Repeater.types';
 
+function propsValidator(
+  props: Omit<RepeaterPropTypes, 'htmlTag' | 'children'>,
+) {
+  const errors = [
+    ...validator(props.replicableProps).Object().errors,
+    ...validator(props.specificProps).Array().errors,
+    ...validator(props.startsWith).Range().errors,
+    ...validator(props.min).Range().errors,
+    ...validator(props.max).Range().errors,
+  ];
+  if (errors.length) {
+    throw new Error(errors.join(','));
+  }
+}
+
 export default function Repeater(props: RepeaterPropTypes) {
+  const { htmlTag, ...otherProps } = props;
+  propsValidator(otherProps);
   const {
-    children,
-    htmlTag,
     replicableProps = {},
     specificProps = [],
     max = Number.MAX_SAFE_INTEGER,
     min = 0,
     startsWith = 1,
-  } = props;
-  React.createElement();
-  return <></>;
+  } = otherProps;
+
+  const allElements = Array(startsWith).map(() => React.createElement(htmlTag));
+  return <>{allElements}</>;
 }
